c2fe5e94c5344e00142a573632415fdf4a24d79f
Brady Shinners <brady.shinners@gmail.com>
2018-08-12 14:23:01 -0500

    heroku jawsDB call

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 // Setup the code to connect Node to MySQL.
 var mysql = require("mysql");
+var connection;
 
-// Create object with mysql connection data
-var sqlConnect = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'burger_db'
-});
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'burger_db'
+    });
+};
 
 // Connect to specified server using object data provided.
-sqlConnect.connect(function(err) {
+sqlConnect.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
