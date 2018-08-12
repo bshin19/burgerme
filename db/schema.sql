bd83f3ec1074fc8d096c9f990008072ca6a3117d
Brady Shinners <brady.shinners@gmail.com>
2018-08-12 16:22:53 -0500

    heroku jaws ref

--- db/schema.sql
+++ db/schema.sql
@@ -1,21 +1,19 @@
--- Create the burgers_db.
-DROP DATABASE if exists burger_db;
-CREATE DATABASE burger_db;
--- Switch to or use the burgers_db.
-USE burger_db;
+USE ifc1ub9hwqlxfggq;
 -- Create a burgers table with these fields:
 
 CREATE TABLE burgers
 (
     -- id: an auto incrementing int that serves as the primary key.
-    id int NOT NULL auto_increment,
+    id INT AUTO_INCREMENT NOT NULL,
 
     -- burger_name: a string.
-    burger_name varchar(50) NOT NULL,
+    burger_name VARCHAR(50) NOT NULL,
 
     -- devoured: a boolean.
-    devoured boolean default false,
+    devoured BOOLEAN DEFAULT FALSE,
+    
+    createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    primary key (id)
+    PRIMARY KEY(id)
 
 );
